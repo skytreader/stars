7c608d282a016c32395f21d548a5b267fdcc3542
Chad Estioco <chadestioco@gmail.com>
2015-10-07 20:18:52 +0800

    This could be para-para-parallax.

--- stars.js
+++ stars.js
@@ -1,4 +1,6 @@
 var STAR_COUNT = 1888;
+var STAR_COLORS = ["#fff", "#e44b4b", "#9093f4", "#f3f490"];
+var DEPTHS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
 function getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
@@ -7,24 +9,28 @@ function getRandomInt(min, max){
 function createStar(r, c){
     var height = $(document).height();
     var width = $(document).width();
+    var li = document.createElement("li");
     var star = document.createElement("div");
 
     var row = getRandomInt(0, height);
     var col = getRandomInt(0, width);
     var boxBlur = getRandomInt(0, 10);
     var boxRadius = getRandomInt(0, boxBlur);
+    var color = STAR_COLORS[getRandomInt(0, STAR_COLORS.length)];
 
     $(star).addClass("star");
+    $(li).addClass("layer").attr("data-depth", DEPTHS[getRandomInt(0, DEPTHS.length)]);
     $(star).css({
         top: row,
         left: col,
-        "box-shadow": "0px 0px " + boxBlur + "px " + boxRadius + "px #fff"
+        "box-shadow": "0px 0px " + boxBlur + "px " + boxRadius + "px " + color,
+        "background-color": "#fff"
     });
-    return star;
+    li.appendChild(star);
+    return li;
 }
 
-$(document).ready(function(){
-    for(var i = 0; i < STAR_COUNT; i++){
-        $("#sky").append(createStar());
-    }
-});
+for(var i = 0; i < STAR_COUNT; i++){
+    $("#sky").append(createStar());
+}
+var par = new Parallax(document.getElementById("sky"));
